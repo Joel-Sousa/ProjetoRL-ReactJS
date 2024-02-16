1e450578e66c5704a4726a5a8895d6c9370662d5
Joel-Sousa <softmock@gmail.com>
2024-02-15 22:48:45 -0300

    Ajuste

--- src/app/layout/header/Header.tsx
+++ src/app/layout/header/Header.tsx
@@ -1,11 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { AppBar, Toolbar, CssBaseline, Typography} from "@mui/material";
+import { AppBar, Toolbar, CssBaseline, Typography, IconButton } from "@mui/material";
 import { makeStyles } from '@mui/styles'
 import { Link, useNavigate } from "react-router-dom";
-import { Button } from '@mui/material';
 import loginService from '../../pages/login/login.service';
 import styles from './header.module.css';
 import { UserContext } from '../../contexts/UserContext';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toast from '../../components/Toast/Toast';
+
 
 const useStyles = makeStyles(() => ({
     header: {
@@ -27,18 +31,46 @@ const useStyles = makeStyles(() => ({
 }));
 
 export default function Header() {
-    
+    const ITEM_HEIGHT = 48;
+
     const classes = useStyles();
-    
+
     const { isToken, roles_id, userData } = useContext(UserContext);
-    
+
+    const [toastMessage, setToastMessage] = useState('');
+    const [toastIcon, setToastIcon] = useState('');
+    const [isToast, setIsToast] = useState(false);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
     const exit = async () => {
+        
         const resp = await loginService.logout();
+
+        setToastMessage('Ate logo!');
+            setToastIcon('success');
+            setIsToast(true);
+
+            handleClose();
+
+            setTimeout(() => {
+                setIsToast(false);
+            },100)
     }
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <>
+            <Toast open={isToast} icon={toastIcon} mensagem={toastMessage} timer={3000} />
+
             <div className={styles.header}>
+                
                 <AppBar position="static">
                     <CssBaseline />
                     <Toolbar className={classes.header}>
@@ -49,33 +81,62 @@ export default function Header() {
                                 <Link to='/' className={classes.link}>Sys</Link>
                             }
                         </Typography>
-                        <div className={classes.navlinks}>
-                            <>
+                        
+                        <div>
+                            <IconButton
+                                aria-label="more"
+                                id="long-button"
+                                aria-controls={open ? "long-menu" : undefined}
+                                aria-expanded={open ? "true" : undefined}
+                                aria-haspopup="true"
+                                onClick={handleClick}
+                                style={{ color: 'white' }}
+                            >
+                                <MoreVertIcon />
+                            </IconButton>
+                            <Menu
+                                id="long-menu"
+                                MenuListProps={{
+                                    "aria-labelledby": "long-button",
+                                }}
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                                PaperProps={{
+                                    style: {
+                                        maxHeight: ITEM_HEIGHT * 4.5,
+                                        width: "20ch",
+                                    },
+                                }}
+                            >
                                 {isToken ?
-                                    <>
+                                    <div>
                                         {roles_id === 1 &&
-                                            <Link to='/listUser'>
-                                                <Button variant="contained" >Listar usuarios</Button>
+                                            <Link to='/listUser' style={{ textDecoration: 'none', color: 'black' }}>
+                                                <MenuItem onClick={handleClose}>
+                                                    Listar usuarios
+                                                </MenuItem>
                                             </Link>
                                         }
-                                        &nbsp;
-                                        <span className={classes.link}>
-                                            <Button className={classes.link} variant="contained" onClick={exit} >Sair</Button>
-                                        </span>
-                                    </>
+                                        <MenuItem onClick={exit}>
+                                            Sair
+                                        </MenuItem>
+                                    </div>
                                     :
-                                    <>
-                                        <Link to='/createUser'>
-                                            <Button className='w-100' variant="contained" >+Usuario</Button>
+                                    <div>
+                                        <Link to='/createUser' style={{ textDecoration: 'none', color: 'black' }}>
+                                            <MenuItem onClick={handleClose}>
+                                                Criar Usuario
+                                            </MenuItem>
                                         </Link>
-                                        &nbsp;
-                                        {/* <Button className='w-100' variant="contained" onClick={handleClickOpen}>Login</Button> */}
-                                        <Link to='/login'>
-                                            <Button className='w-100' variant="contained" >Login</Button>
+                                        <Link to='/login' style={{ textDecoration: 'none', color: 'black' }}>
+                                            <MenuItem onClick={handleClose}>
+                                                Login
+                                            </MenuItem>
                                         </Link>
-                                    </>
+                                    </div>
                                 }
-                            </>
+                            </Menu>
                         </div>
                     </Toolbar>
                 </AppBar>
